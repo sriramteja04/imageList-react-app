0b1f58f8682a3686c0408bfe4cc43207f89740ac
sriramteja04 <s531524@nwmissouri.edu>
2019-06-15 05:46:58 -0500

    updated the images view

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import classes from './Images.module.css';
+import ImageCard from '../ImageCard';
 
 const Images = props => {
-  const images = props.results.map(({ id, urls, description }) => (
-    <img key={id} src={urls.regular} alt={description} />
+  const images = props.results.map(result => (
+    <ImageCard key={result.id} image={result} />
   ));
 
   return <div className={classes.ImageList}>{images}</div>;
